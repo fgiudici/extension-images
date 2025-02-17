485ee74bb0cfa960ce58eecef9042c8d9e77bb39
Francesco Giudici <francesco.giudici@suse.com>
2025-02-17 19:09:08 +0100

    scripts: rke2 script now creates a container...
    ...carrying the sysextimg rke2 raw image
    
    Signed-off-by: Francesco Giudici <francesco.giudici@suse.com>

--- scripts/rke22ext.sh
+++ scripts/rke22ext.sh
@@ -2,6 +2,34 @@
 
 set -e
 
+print_help() {
+	echo "Usage: $0 [-k \$RKE2_VER] [-a \$TARGET_ARCH] [-i \$ID] [-v \$VERSION_ID] [-t \$TAG] [-dh]"
+	echo "     -k: rke2 version to package ($RKE2_VER)"
+	echo "     -a: architecture of the rke2 binary ($TARGET_ARCH)"
+	echo "     -i: ID to be used for the system external image ($ID)"
+	echo "     -v: ID_VERSION to be used for the system external image ($ID_VERSION)"
+	echo "     -d: debug (leaves around the directories for the build process)"
+	echo "     -t: tagged name to apply to the generated container image"
+	echo "     -r: repo name to prepend to the container image"
+	exit
+}
+
+REPO="localhost"
+KEEP_TMP=0
+while getopts k:a:i:v:t:r:dh opt
+do
+	case $opt in
+	k) RKE2_VER="$OPTARG" ;;
+	a) TARGET_ARCH="$OPTARG" ;;
+	i) ID="$OPTARG" ;;
+	v) VERSION_ID= ;;
+	d) KEEP_TMP=1 ;;
+	t) TAG="$OPTARG" ;;
+	r) REPO="$OPTARG" ;;
+	?) print_help ;;
+	esac
+done
+
 : ${TARGET_ARCH:="amd64"}
 : ${RKE2_VER:="v1.32.1+rke2r1"}
 
@@ -9,21 +37,17 @@ set -e
 : ${VERSION_ID:="6.1"}
 : ${EXTENSION_RELOAD_MANAGER:="1"}
 
+TMPTAG1=${RKE2_VER/v}
+TMPTAG1=${TMPTAG1%+*}
+TMPTAG2=${RKE2_VER/*+}
+TMPTAG1=${TMPTAG1}.${TMPTAG2}
+: ${TAG:="sysextimg-rke2:$TMPTAG1"}
+
 : ${RKE2_DIR:="$RKE2_VER"}
 : ${TEMPDIR:=$(mktemp -d -p $PWD temp-$RKE2_VER.XXXXXX)}
 
-print_help() {
-	echo "Usage: $0 [-k \$RKE2_VER] [-a \$TARGET_ARCH] [-i \$ID] [-v \$VERSION_ID] [-dh]\n"
-	echo "     -k: rke2 version to package ($RKE2_VER)"
-	echo "     -a: architecture of the rke2 binary ($TARGET_ARCH)"
-	echo "     -i: ID to be used for the system external image ($ID)"
-	echo "     -v: ID_VERSION to be used for the system external image ($ID_VERSION)"
-	echo "     -d: debug (leaves around the directories for the build process)"
-	exit
-}
-
 prereq_checks() {
-	local cmd_list="tar chcon mksquashfs"
+	local cmd_list="tar cat chcon mksquashfs"
 	for cmd in $cmd_list; do
 		if ! command -v "$cmd" > /dev/null; then
 			echo "'$cmd' not found."
@@ -31,19 +55,6 @@ prereq_checks() {
 		fi
 	done
 }
-
-KEEP_TMP=0
-while getopts k:t:i:v:dh opt
-do
-	case $opt in
-	k) RKE2_VER="$OPTARG" ;;
-	t) TARGET_ARCH="$OPTARG" ;;
-	i) ID="$OPTARG" ;;
-	v) VERSION_ID= ;;
-	d) KEEP_TMP=1 ;;
-	?) print_help ;;
-	esac
-done
 
 get_rke2_archive() {
 	pushd ${TEMPDIR}
@@ -68,7 +79,7 @@ make_rke2_squashfs() {
 	cp $TEMPDIR/bin/rke2 usr/local/bin/
 	cp $TEMPDIR/lib/systemd/system/* usr/lib/systemd/system/
 
-	/bin/cat <<EOF >usr/lib/extension-release.d/extension-release.${RKE2_DIR}
+	cat <<EOF >usr/lib/extension-release.d/extension-release.${RKE2_DIR}
 ID=${ID}
 VERSION_ID=${VERSION_ID}
 EXTENSION_RELOAD_MANAGER=${EXTENSION_RELOAD_MANAGER}
@@ -88,12 +99,18 @@ EOF
 	mv $TEMPDIR/$RKE2_DIR.raw ./
 }
 
+create_container_image() {
+	printf "FROM scratch\nADD ${RKE2_DIR}.raw /\n" > Dockerfile
+	podman build . -t $REPO/$TAG
+}
+
 prereq_checks
 get_rke2_archive
 make_rke2_squashfs
+create_container_image
 
 if [ $KEEP_TMP = 0 ]; then
-	rm -rf  $TEMPDIR
+	rm -rf  $TEMPDIR Dockerfile
 fi
 
 
