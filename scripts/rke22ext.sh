f7689e0b2fbec9829f92d4fabf8be692b5e31c49
Francesco Giudici <francesco.giudici@suse.com>
2025-02-17 18:10:50 +0100

    scripts: improve rke2 image build script
    * Make the temp folder have a randomly generated name
    * Add functions
    * Check required binaries are present
    
    Signed-off-by: Francesco Giudici <francesco.giudici@suse.com>

--- scripts/rke22ext.sh
+++ scripts/rke22ext.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+set -e
+
 : ${TARGET_ARCH:="amd64"}
 : ${RKE2_VER:="v1.32.1+rke2r1"}
 
 : ${ID:="sl-micro"}
 : ${VERSION_ID:="6.1"}
 : ${EXTENSION_RELOAD_MANAGER:="1"}
+
+: ${RKE2_DIR:="$RKE2_VER"}
+: ${TEMPDIR:=$(mktemp -d -p $PWD temp-$RKE2_VER.XXXXXX)}
 
 print_help() {
 	echo "Usage: $0 [-k \$RKE2_VER] [-a \$TARGET_ARCH] [-i \$ID] [-v \$VERSION_ID] [-dh]\n"
@@ -17,6 +22,16 @@ print_help() {
 	exit
 }
 
+prereq_checks() {
+	local cmd_list="tar chcon mksquashfs"
+	for cmd in $cmd_list; do
+		if ! command -v "$cmd" > /dev/null; then
+			echo "'$cmd' not found."
+			return 1
+		fi
+	done
+}
+
 KEEP_TMP=0
 while getopts k:t:i:v:dh opt
 do
@@ -26,46 +41,59 @@ do
 	i) ID="$OPTARG" ;;
 	v) VERSION_ID= ;;
 	d) KEEP_TMP=1 ;;
-	?) print_help ;; 
+	?) print_help ;;
 	esac
 done
-: ${RKE2_DIR:="$RKE2_VER"}
 
-mkdir temp
-pushd temp
-if [ -f rke2.linux-amd64.tar.gz ]; then
-	echo "RKE2 tar.gz file found under ./temp: assuming is at version $RKE2_VER"
-	echo "if not please delete the rke2.linux-amd64.tar.gz file before running this script"
-	read -p "press a key to continue or CTRL-c to stop."
-else
-	wget https://github.com/rancher/rke2/releases/download/${RKE2_VER}/rke2.linux-amd64.tar.gz
-fi
+get_rke2_archive() {
+	pushd ${TEMPDIR}
+	if [ -f rke2.linux-amd64.tar.gz ]; then
+		echo "RKE2 tar.gz file found under $TEMPDIR: assuming is at version $RKE2_VER"
+		read -p "press a key to continue or CTRL-c to stop."
+	else
+		wget https://github.com/rancher/rke2/releases/download/${RKE2_VER}/rke2.linux-amd64.tar.gz
+	fi
 
-tar xvf rke2.linux-amd64.tar.gz
-popd
+	tar xvf rke2.linux-amd64.tar.gz
+	popd
+}
 
-mkdir -p ${RKE2_DIR}/usr/local/bin
-mkdir -p ${RKE2_DIR}/usr/lib/systemd/system
-mkdir -p ${RKE2_DIR}/usr/lib/extension-release.d
-cp temp/bin/rke2 ${RKE2_DIR}/usr/local/bin/
-cp temp/lib/systemd/system/* ${RKE2_DIR}/usr/lib/systemd/system/
+make_rke2_squashfs() {
+	pushd ${TEMPDIR}
+	mkdir ${RKE2_DIR}
+	pushd ${RKE2_DIR}
+	mkdir -p usr/local/bin
+	mkdir -p usr/lib/systemd/system
+	mkdir -p usr/lib/extension-release.d
+	cp $TEMPDIR/bin/rke2 usr/local/bin/
+	cp $TEMPDIR/lib/systemd/system/* usr/lib/systemd/system/
 
-pushd $RKE2_DIR
-/bin/cat <<EOF >usr/lib/extension-release.d/extension-release.${RKE2_DIR}
+	/bin/cat <<EOF >usr/lib/extension-release.d/extension-release.${RKE2_DIR}
 ID=${ID}
 VERSION_ID=${VERSION_ID}
 EXTENSION_RELOAD_MANAGER=${EXTENSION_RELOAD_MANAGER}
 EOF
 
-chcon -R system_u:object_r:usr_t:s0 usr
-chcon -R system_u:object_r:lib_t:s0 usr/lib
-chcon -R system_u:object_r:systemd_unit_file_t:s0  usr/lib/systemd/system
-chcon -R system_u:object_r:bin_t:s0 usr/local/bin
+	chcon -R system_u:object_r:usr_t:s0 usr
+	chcon -R system_u:object_r:lib_t:s0 usr/lib
+	chcon -R system_u:object_r:systemd_unit_file_t:s0  usr/lib/systemd/system
+	chcon -R system_u:object_r:bin_t:s0 usr/local/bin
 
-popd
+	popd
 
-mksquashfs $RKE2_DIR $RKE2_DIR.raw -all-root
+	rm -f $RKE2_DIR.raw
+	mksquashfs $RKE2_DIR $RKE2_DIR.raw -all-root
+
+	popd
+	mv $TEMPDIR/$RKE2_DIR.raw ./
+}
+
+prereq_checks
+get_rke2_archive
+make_rke2_squashfs
 
 if [ $KEEP_TMP = 0 ]; then
-	rm -rf $RKE2_DIR temp
+	rm -rf  $TEMPDIR
 fi
+
+
